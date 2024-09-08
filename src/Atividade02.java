09c1393a40fd50fc7b6c903c62f799b9cd514c21
gusta <gustavomullerdesouza@gmail.com>
2024-09-08 16:42:17 -0300

    Vetores

--- src/Atividade02.java
+++ src/Atividade02.java
@@ -2,14 +2,14 @@ public class Atividade02 {
     public static void main(String[] args) {
 
      int[] numeros = new int[4];
-     double valorFinal;
+     int valorFinal;
 
      numeros[0] = 3;
      numeros[1] = 6;
      numeros[2] = 9;
      numeros[3] = 12;
 
-     valorFinal = numeros[0] + numeros[4];
+     valorFinal = numeros[0] + numeros[3];
 
      System.out.println("Soma do Primeiro e Ultimo é : " + valorFinal);
 
